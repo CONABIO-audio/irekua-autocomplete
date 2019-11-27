cc00e87454d837c4a53614ef8ba3ef6282ef32d7
mbsantiago <santiago.mbal@gmail.com>
2019-11-27 14:51:15 -0600

    updated repository field in setup

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     license='BSD License',
     description='Autocomplete endpoints for Irekua type models',
     long_description=README,
-    url='http://selia.conabio.gob.mx/',
+    url='https://github.com/CONABIO-audio/irekua-autocomplete',
     author='CONABIO, Gustavo Everardo Robredo Esquivelzeta, Santiago Martínez Balvanera',
     author_email='erobredo@conabio.gob.mx, smartinez@conabio.gob.mx',
     install_requires=[
